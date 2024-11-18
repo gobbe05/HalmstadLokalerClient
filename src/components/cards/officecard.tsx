cc4bb320c693e6707de098e6303ccd1412e251bd
Gabriel Raskov <gabriel@raskov.se>
2024-11-18 19:53:21 +0100

    Updated cards to accept office parameter as IOffice

--- src/components/cards/officecard.tsx
+++ src/components/cards/officecard.tsx
@@ -1,22 +1,32 @@
-export default function OfficeCard({location,price, size, image}: {location: string, price: number, size: number, image: string}) {
-    return (
-        <div className="w-full bg-white text-gray-700 border border-gray-300 shadow-lg rounded-lg overflow-hidden">
-            <img className="w-full" src={`http://${import.meta.env.VITE_SERVER_IP}:${import.meta.env.VITE_SERVER_PORT}`+image}/>
-            <div className="p-4">
-                <h3 className="text-xl font-semibold">{location}</h3>
-            <p className="text-sm font-light mt-1">Detta är en liten kort beskrivning om kontoret</p>
-                <div className="flex mt-4">
-                    <div className="w-1/2">
-                        <p className="font-light text-sm">Price</p>
-                        <p className="font-semibold">{price} kr/mån</p>
-                    </div>
-                    <div className="w-1/2">
-                        <p className="font-light text-sm">Yta</p>
-                        <p className="font-semibold">{size} m2</p>
-                    </div>
-                </div>
-            </div>
-            
+import IOffice from "../../interfaces/IOffice";
+
+export default function OfficeCard({office}: {office: IOffice}) {
+  const serverUrl = `http://${import.meta.env.VITE_SERVER_IP}:${import.meta.env.VITE_SERVER_PORT}`;
+  
+  return (
+    <div className="w-full bg-white text-gray-700 border shadow-md rounded-lg overflow-hidden transition-transform transform">
+      <img 
+        className="w-full h-[150px] object-cover" 
+        src={`${serverUrl}${office.image}`} 
+        alt={`Bild på lokal i ${location}`} 
+      />
+      <div className="p-4">
+        <h3 className="text-xl font-semibold">{office.location}</h3>
+        <p className="text-sm font-light mt-1">
+          Detta är en liten kort beskrivning om kontoret
+        </p>
+
+        <div className="flex mt-4">
+          <div className="w-1/2">
+            <p className="font-light text-sm">Pris</p>
+            <p className="font-semibold">{office.price.toLocaleString()} kr/mån</p>
+          </div>
+          <div className="w-1/2">
+            <p className="font-light text-sm">Yta</p>
+            <p className="font-semibold">{office.size} m<sup>2</sup></p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/cards/officecardlong.tsx
+++ src/components/cards/officecardlong.tsx
@@ -1,15 +1,16 @@
+import IOffice from "../../interfaces/IOffice";
 import ContactButton from "../buttons/contactbutton";
 
-export default function OfficeCardLong({location, price, size, image}: {location: string, price: number, size: number, image: string}) {
+export default function OfficeCardLong({office}: {office: IOffice}) {
     return (
-        <div className="w-full flex bg-white text-gray-700 border border-gray-300 rounded-md shadow-lg overflow-hidden">
+        <div className="w-full flex bg-white text-gray-700 border rounded-md shadow overflow-hidden">
             <div>
-                <img className="h-full" src={`http://${import.meta.env.VITE_SERVER_IP}:${import.meta.env.VITE_SERVER_PORT}`+image}/>
+                <img className="h-full" src={`http://${import.meta.env.VITE_SERVER_IP}:${import.meta.env.VITE_SERVER_PORT}`+office.image}/>
             </div>
             <div className="w-full p-4">
-                <div className="text-gray-700"> <h1 className="text-xl font-semibold">{location}</h1>
+                <div className="text-gray-700"> <h1 className="text-xl font-semibold">{office.location}</h1>
                     <p className="text-sm font-light">Name of this place</p>
-                    <p className="font-xl font-bold mt-2">{size} m2</p>
+                    <p className="font-xl font-bold mt-2">{office.size} m2</p>
                     <p className="h-16 mt-4 text-sm text-ellipsis overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nisl sed nulla tempus vehicula. Duis lobortis sagittis fermentum. Quisque faucibus, justo non faucibus malesuada, magna neque dapibus magna, vel mattis arcu est feugiat eros. Aliquam dapibus enim velit, eu pellentesque tortor sollicitudin in. Mauris dignissim metus ligula, at molestie quam dictum non.</p>
                 </div>
                 <div className="flex items-center mt-4">

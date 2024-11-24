4dd1697bd9df78a3d759ee50889f04b1b3851247
Gabriel Raskov <gabriel@raskov.se>
2024-11-24 21:04:17 +0100

    Implemented react query client

--- src/App.tsx
+++ src/App.tsx
@@ -12,29 +12,40 @@ import LedigaLokaler from "./components/pages/lediga-lokaler/ledigalokaler";
 import HyrUtLokal from "./components/pages/hyr-ut-lokal/hyrutlokal";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import Bevakningar from "./components/pages/bevakningar/bevakningar";
+import Meddelanden from "./components/pages/meddelanden/meddelanden";
+import Konversation from "./components/pages/konversation/konversation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Router>
-        <ToastContainer />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/hyr-ut-lokal" element={<ProtectedRoute />}>
-              <Route index element={<HyrUtLokal />} />
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <ToastContainer />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/hyr-ut-lokal" element={<ProtectedRoute />}>
+                <Route index element={<HyrUtLokal />} />
+              </Route>
+              <Route path="/lediga-lokaler">
+                <Route index element={<LedigaLokaler />} />
+              </Route>
+              <Route path="/bevakningar">
+                <Route index element={<Bevakningar />}/>
+              </Route>
+              <Route path="/meddelanden" element={<ProtectedRoute />}>
+                <Route index element={<Meddelanden />}/>
+                <Route path="/meddelanden/:id" element={<Konversation />} />
+              </Route>
             </Route>
-            <Route path="/lediga-lokaler">
-              <Route index element={<LedigaLokaler />} />
-            </Route>
-            <Route path="/bevakningar">
-              <Route index element={<Bevakningar />}/>
-            </Route>
-          </Route>
-        </Routes>
-      </Router>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </AuthProvider>
   );
 };

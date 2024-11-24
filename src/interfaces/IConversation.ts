7c975b607b608af3fe7a91d2202adaf741f3f50b
Gabriel Raskov <gabriel@raskov.se>
2024-11-24 21:03:40 +0100

    Created interfaces for message functionality

---
+++ src/interfaces/IConversation.ts
@@ -0,0 +1,5 @@
+export default interface IConversation {
+    _id: string;
+    broker: string;
+    buyer: string;
+}
---
+++ src/interfaces/IMessage.ts
@@ -0,0 +1,6 @@
+export default interface IMessage {
+    _id: string;
+    conversation: string;
+    sender: string;
+    message: string;
+}

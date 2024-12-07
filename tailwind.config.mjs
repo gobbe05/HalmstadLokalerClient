14433c7cca8e8c4e3fcdbfcd9b8dc8c0a0fe1d85
Gabriel Raskov <gabriel@raskov.se>
2024-12-07 12:00:16 +0100

    updatead tailwindconfig to fix errors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -10,4 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};

c8fe58a8dfeb20e5ec85d31106fe2d2d82d71a0a
Gabriel Raskov <gabriel@raskov.se>
2024-12-21 15:28:32 +0100

    Upgraded Loading component and implementation in auth

--- src/components/layout/loading.tsx
+++ src/components/layout/loading.tsx
@@ -1,5 +1,9 @@
+import {Oval} from "react-loader-spinner"
+
 export default function Loading() {
     return (
-        <>Loading...</>
+        <div className="w-full h-full flex justify-center items-center">
+            <Oval />
+        </div>
     )
 }
--- src/context/Auth/AuthContext.tsx
+++ src/context/Auth/AuthContext.tsx
@@ -2,14 +2,16 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from "react";
 import register from "./register";
 import initializeAuth from "./initializeAuth";
+import Loading from "../../components/layout/loading";
 
 interface AuthContextType {
   isAuthenticated: boolean;
   authId: string | undefined,
+  type: string | undefined,
   isLoading: boolean,
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
   login: (username: string, password: string) => Promise<number>;
-  register: (email: string, username: string, password: string, confirmPassword: string) => Promise<number | Error>;
+  register: (email: string, username: string, password: string, confirmPassword: string, type: "buyer" | "seller") => Promise<number | Error>;
   logout: () => Promise<void>;
 }
 
@@ -18,11 +20,12 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [authId, setAuthId] = useState<string | undefined>(undefined)
+  const [type, setType] = useState<string | undefined>("")
   const [isLoading, setIsLoading] = useState(true)
 
   // Check authentication status on initial load
   useEffect(() => {
-    initializeAuth(setIsAuthenticated, setIsLoading, setAuthId)
+    initializeAuth(setIsAuthenticated, setIsLoading, setAuthId, setType)
   }, []);
   
   const login = async (username: string, password: string): Promise<number> => {
@@ -38,6 +41,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
         const data = await response.json()   
         setIsAuthenticated(true)
         setAuthId(data._id)
+        setType(data.type)
         localStorage.setItem("isAuthenticated", "true"); // Save to localStorage
         return response.status
     }
@@ -49,13 +53,15 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     })
     if(response.status == 200) {
         setIsAuthenticated(false)
+        initializeAuth(setIsAuthenticated, setIsLoading, setAuthId, setType)
         localStorage.removeItem("isAuthenticated"); // Remove from localStorage
     }
   };
   
-
+  if(isLoading)
+    return <Loading />
   return (
-    <AuthContext.Provider value={{ isAuthenticated, authId, setIsAuthenticated, logout, login, register, isLoading }}>
+    <AuthContext.Provider value={{ isAuthenticated, authId, type, setIsAuthenticated, logout, login, register, isLoading }}>
       {children}
     </AuthContext.Provider>
   );
@@ -63,6 +69,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
+  console.log(context)
   if (!context) {
     throw new Error("useAuth must be used within an AuthProvider");
   }

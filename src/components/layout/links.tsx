fa2cf307e5431065430fa3a8f5d565f83fed4456
Gabriel Raskov <gabriel@raskov.se>
2024-12-30 14:03:06 +0100

    Sidopanel för mindre skärmar

--- src/components/layout/links.tsx
+++ src/components/layout/links.tsx
@@ -9,18 +9,18 @@ import { IconType } from "react-icons";
 export default function Links() {
     const {logout, isAuthenticated, type} = useAuth();
     return (
-        <div className="flex items-stretch gap-6 text-gray-700">
+        <div className="flex flex-col-reverse md:flex-row items-stretch gap-6">
             {type != "buyer" && <NewOfficeButton link="/hyr-ut-lokal"/>}
             <HeaderLink text="Lediga lokaler" link="/lediga-lokaler" Icon={HiOutlineSearch}/>
             <HeaderLink text="Bevakningar" link="/bevakningar" Icon={HiOutlineBell}/>
             
             {isAuthenticated ? 
             <>
-                <Link to="/meddelanden" className="hover:text-blue-500 transition-colors duration-300"><HiOutlineChatBubbleLeftEllipsis className="mx-auto" size={24}/><p className="text-sm">Meddelanden</p></Link>   
-                {type != "buyer" && <Link to="/min-sida" className="hover:text-blue-500 transition-colors duration-300"><HiOutlineUserCircle className="mx-auto" size={24}/><p className="text-sm">Min Sida</p></Link>}
-                <button className="hover:text-red-500 transition-colors duration-300" onClick={logout}><HiOutlineLogout className="mx-auto" size={24}/><p className="text-sm">Logout</p></button>
+                <HeaderLink link="/meddelanden" text="Meddelanden" Icon={HiOutlineChatBubbleLeftEllipsis }/>  
+                {type != "buyer" && <HeaderLink link="/min-sida" text="Min Sida" Icon={HiOutlineUserCircle} /> }
+                <button className="flex md:flex-col gap-2 md:gap-0 hover:text-red-500 transition-colors duration-300" onClick={logout}><HiOutlineLogout className="md:mx-auto" size={24}/><p className="text-sm">Logout</p></button>
             </> : 
-            <Link to="/login" className="hover:text-blue-500 transition-colors duration-300"><HiOutlineUserCircle className="mx-auto" size={24}/><p className="text-sm">Logga in</p></Link>}
+            <HeaderLink link="/login" text="Logga in" Icon={HiOutlineUserCircle }/>}
             
             {/*<Link to="/meddelanden"><HiOutlineChatBubbleBottomCenterText className="mx-auto" size={24}/><p className="text-sm">Meddelanden</p></Link>*/}
         </div>
@@ -35,6 +35,6 @@ type HeaderLinkProps = {
 
 const HeaderLink = ({text, link, Icon}: HeaderLinkProps) => {
     return (
-        <Link to={link} className="flex flex-col items-center text-gray-700 hover:text-blue-500 transition-colors duration-300"><Icon className="mx-auto" size={24}/><p className="text-sm">{text}</p></Link>
+        <Link to={link} className="flex md:flex-col gap-2 md:gap-0 items-center text-gray-700 hover:text-blue-500 transition-colors duration-300"><Icon className="md:mx-auto" size={24}/><p className="text-sm">{text}</p></Link>
     )
 }

d922b8aac3565cd11f081c0278295e586fe50e3e
Gabriel Raskov <gabriel@raskov.se>
2024-12-31 09:51:06 +0100

    Temporary bug fix

--- src/components/pages/sparade-lokaler/sparadelokaler.tsx
+++ src/components/pages/sparade-lokaler/sparadelokaler.tsx
@@ -17,12 +17,12 @@ const SparadeLokaler = () => {
 
     return (
         <div className="flex flex-col w-2/3 mx-auto text-gray-700 bg-white p-16 my-32 rounded"> 
-            {!data.offices.length && 
-            <div className="text-center mt-4 py-8">
+            {!error && !isPending && !data.offices.length ? <div className="text-center mt-4 py-8">
                 <h3 className="text-2xl font-semibold">Här var det tomt...</h3>
                 <p className="mt-1 text-lg">Spara en lokal och kom tillbaks</p>
-            </div>}
-            {data.offices[0] && <h1 className="text-2xl font-semibold text-center">Dina sparade lokaler</h1>}
+            </div>
+            :
+            <h1 className="text-2xl font-semibold text-center">Dina sparade lokaler</h1>}
             <div className="flex flex-col gap-8 mt-16 mx-16">
                 {error || isPending ? <Loading /> : data.offices.map((savedOffice: ISavedOffice) => <OfficeCardLong key={savedOffice._id} office={savedOffice.office}/>)}
             </div>

b3e305fa478fa4ef4d2d5ae8bcf559228a31445c
Gabriel Raskov <gabriel@raskov.se>
2025-06-17 21:18:25 +0200

    Showing new fields on profile page

--- src/components/pages/profile/profile.tsx
+++ src/components/pages/profile/profile.tsx
@@ -35,22 +35,24 @@ const ProfileCard = ({openProfile, setOpenProfile, id}: {openProfile: boolean, s
         if (boxRef.current && !boxRef.current.contains(event.target as Node)) {
             setOpenProfile(false);
         }
-    }} className={`w-screen h-screen fixed left-0 top-0 bg-gray-500 bg-opacity-50 ${!openProfile && "hidden" }`}>
+    }} className={`w-screen h-screen fixed z-10 left-0 top-0 bg-gray-500 bg-opacity-50 ${!openProfile && "hidden" }`}>
         <div ref={boxRef} className="bg-white rounded-2xl shadow-lg max-w-2xl overflow-hidden w-full p-8 mx-auto mt-10">
             <div className="flex justify-between items-center mb-4">
                 <button onClick={() => setOpenProfile(false)} className="text-gray-700 bg-white hover:bg-gray-700 hover:text-white p-1 rounded-full transition-all ml-auto"><FaTimes size={24}/></button>
             </div>
             <div className="flex flex-col items-center">
                 <img src={`https://api.dicebear.com/5.x/initials/svg?seed=${profileData && profileData.user.username}`} alt="Profile" className="w-32 h-32 rounded-full border border-gray-300" />
-                <h2 className="text-2xl font-bold text-gray-800">{profileData && profileData.user.username}</h2>
-                <p className="text-sm text-gray-500 mb-10">{profileData && profileData.user.email}</p>
+                <h2 className="text-2xl font-bold text-gray-800">{profileData && profileData.user.firstName + profileData.user.lastName}</h2>
+                <p className="text-sm text-gray-500 mb-10">{profileData && "@"+profileData.user.username}</p>
 
-                {/*<div className="text-center text-gray-700 mb-6">
-                    <p><strong>Email:</strong> {profile.email}</p>
-                    <p><strong>Phone:</strong> {profile.phone}</p>
-                </div> */} 
+                {<div className="text-center text-gray-700 mb-6">
+                    <p><strong>Email:</strong> {profileData && profileData.user.email}</p>
+                    <p><strong>Företag:</strong> {profileData && profileData.user.companyName}</p>
+                    <p><strong>Org. Nr:</strong> {profileData && profileData.user.orgNr}</p>
+                    <p><strong>Fakturaadress:</strong> {profileData && profileData.user.invoiceAddress}</p>
+                </div>} 
 
-                <div className="w-full overflow-scroll max-h-[40dvh]">
+                <div className={`${!officesData?.offices?.length && "hidden"} w-full overflow-scroll max-h-[40dvh]`}>
                     <h3 className="text-lg font-semibold text-gray-800 mb-2">Offices</h3>
                     <div className="grid gap-4">
                         {officesData && officesData.offices.length > 0 && officesData.offices.map((office) => (
@@ -58,7 +60,7 @@ const ProfileCard = ({openProfile, setOpenProfile, id}: {openProfile: boolean, s
                         ))}
                     </div>
                     <div className="flex justify-center mt-4">
-                        <button onClick={() => {setLimit(prev => prev == null ? 3 : null)}} className="text-gray-700 border border-gray-700 hover:bg-gray-700 hover:text-white rounded-full px-4 py-2 transition-all">{limit == null ? "Visa mindre" : "Visa alla dina annonser"}</button>
+                        <button onClick={() => {setLimit(prev => prev == null ? 3 : null)}} className="text-gray-700 border border-gray-700 hover:bg-gray-700 hover:text-white rounded-full px-4 py-2 transition-all">{limit == null ? "Visa mindre" : "Visa alla annonser"}</button>
                     </div>
 
                 </div>
